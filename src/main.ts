b7bc6151cf7bcebb86bb90fa784286d719cf5a2d
mathzds <pedrofilhodev@gmail.com>
2024-11-22 14:09:32 -0300

    feat: adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true
-  }))
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+		}),
+	);
 
-  await app.listen(3000);
+	const config = new DocumentBuilder()
+		.setTitle("QRFood")
+		.build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, documentFactory);
+
+	await app.listen(3000);
 }
 bootstrap();
